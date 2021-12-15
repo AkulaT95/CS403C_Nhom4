4748c644662975f2b8d20b81ad3320034e67308f
Tuan Dang <dangtuansapper@gmail.com>
2021-12-15 10:08:50 +0700

    alo

---
+++ WebDienThoaiCDIO5 - Copy/WebDienThoaiCDIO5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebDienThoaiCDIO5.Global" Language="C#" %>
---
+++ WebDienThoaiCDIO5/WebDienThoaiCDIO5/default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebDienThoaiCDIO5
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        //string connStr = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+        KetNoi kn = new KetNoi();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+                return;
+            string sqlStr = "select * from Product";
+            DataTable dt = new DataTable();
+            try
+            {
+                dt=kn.layDL(sqlStr);               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            DataList1.DataSource = dt;
+            DataList1.DataBind();
+        }
+    }
+}
